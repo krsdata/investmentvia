00c2843c5b1fd2d77baccb65adea72a426689662
krsdata <krsdata.net@gmail.com>
2017-04-15 23:53:23 +0530

    admin setup

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,42 @@
+  <footer role="contentinfo">
+        <div class="clearfix">
+            <ul class="list-unstyled list-inline pull-left">
+                <li>AVANT &copy; 2015</li>
+            </ul>
+            <button class="pull-right btn btn-inverse-alt btn-xs hidden-print" id="back-to-top"><i class="fa fa-arrow-up"></i></button>
+        </div>
+    </footer>
+
+</div> <!-- page-container -->
+
+<!--
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+
+<script>!window.jQuery && document.write(unescape('%3Cscript src="assets/js/jquery-1.10.2.min.js"%3E%3C/script%3E'))</script>
+<script type="text/javascript">!window.jQuery.ui && document.write(unescape('%3Cscript src="assets/js/jqueryui-1.10.3.min.js'))</script>
+-->
+<script type='text/javascript' src='{{ asset('public/assets/js/jquery-1.10.2.min.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/js/jqueryui-1.10.3.min.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/js/bootstrap.min.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/js/enquire.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/js/jquery.cookie.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/js/jquery.nicescroll.min.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/plugins/codeprettifier/prettify.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/plugins/easypiechart/jquery.easypiechart.min.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/plugins/sparklines/jquery.sparklines.min.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/plugins/form-toggle/toggle.min.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/plugins/fullcalendar/fullcalendar.min.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/plugins/form-daterangepicker/daterangepicker.min.js') }}'></script> 
+<script type='text/javascript' src='assets/{{ asset('public/assets/plugins/form-daterangepicker/moment.min.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/plugins/charts-flot/jquery.flot.min.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/plugins/charts-flot/jquery.flot.resize.min.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/plugins/charts-flot/jquery.flot.orderBars.min.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/plugins/pulsate/jQuery.pulsate.min.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/demo/demo-index.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/js/placeholdr.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/js/application.js') }}'></script> 
+<script type='text/javascript' src='{{ asset('public/assets/demo/demo.js') }}'></script> 
+
+</body>
+</html>
